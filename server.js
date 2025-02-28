81380116a981e12d2ef363d8b995b0c2a486107b
vitrokb <victor.brns2@gmail.com>
2025-02-28 08:58:24 -0300

    feat: change version and add start

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // See https://github.com/typicode/json-server#module
-import jsonServer from 'json-server';
-// const jsonServer = require('json-server');
+// import jsonServer from 'json-server';
+const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
